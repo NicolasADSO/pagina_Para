7e4c8732482d964a4e49955d47053c68f0dc8d70
Nicolas Fernando Araque <nifearsa0719@gmail.com>
2025-07-07 21:16:20 -0500

    detalles nuevos

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
+// Inicializar AOS con duración y animación única
 AOS.init({ duration: 1000, once: true });
 
+// Cambiar color de fondo del navbar al hacer scroll
 window.addEventListener("scroll", function () {
   const nav = document.querySelector(".custom-navbar");
   nav.classList.toggle("scrolled", window.scrollY > 50);
 });
 
-const map = L.map('map').setView([4.705633, -74.228794], 16);
+// Inicializar el mapa Leaflet con ubicación del SENA Mosquera
+const map = L.map('map').setView([4.68965, -74.22001], 17);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap',
+  attribution: '© OpenStreetMap'
 }).addTo(map);
 
-L.marker([4.705633, -74.228794])
+L.marker([4.68965, -74.22001])
   .addTo(map)
-  .bindPopup('Centro Biotecnológico del SENA, Mosquera')
+  .bindPopup('Centro de Biotecnología Agropecuaria - SENA Mosquera')
   .openPopup();
